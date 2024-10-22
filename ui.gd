54bc673153078dce61dbaaabb8182c08989840bf
SnakMan <123SamirDhakal123@gmail.com>
2024-10-22 15:26:54 -0400

    makeing cooldown bar for nuke#

--- ui.gd
+++ ui.gd
@@ -1,11 +1,14 @@
 extends Node3D
 
+var NukeCooldown = 1.5
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	$CanvasLayer/CurrrentWeapon.text = str("Current Weapon: ", Data.weapon_name(Data.current_weapon))
+	NukeCooldown -= delta
+	$CanvasLayer/OrbitalStrikeCooldown.value = NukeCooldown * 100
