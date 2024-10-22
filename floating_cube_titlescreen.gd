014147261991ca5abb20c0916ea9fcb0bf1ab515
quasar098 <70716985+quasar098@users.noreply.github.com>
2024-10-22 15:08:26 -0400

    awefoij

--- floating_cube_titlescreen.gd
+++ floating_cube_titlescreen.gd
@@ -15,9 +15,15 @@ func _process(delta: float) -> void:
 	destructable_process(delta)
 	time += delta
 	var rpos = $RigidBody3D.position
-	$RigidBody3D.position.y = starting_pos.y + 0.4*sin(1.5*time)
+	$RigidBody3D.position.x = starting_pos.x
+	$RigidBody3D.position.y = starting_pos.y + 0.1*sin(1.5*time)
+	$RigidBody3D.position.z = starting_pos.z
 	$RigidBody3D.rotation.y = time/0.3
 	$RigidBody3D.rotation.x = -cos(time)*0.2
 	$RigidBody3D.rotation.z = -sin(time)*0.2
+	var box : BoxShape3D = $RigidBody3D/CollisionShape3D.shape
+	box.size.x = 1.6
+	box.size.y = 1.6
+	box.size.z = 1.6
 
 # todo: do something on destroyed
