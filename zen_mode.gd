2119d38323970f29ac427756463872b858e14a0c
SnakMan <123SamirDhakal123@gmail.com>
2024-10-24 15:19:41 -0400

    zen mode

---
+++ zen_mode.gd
@@ -0,0 +1,12 @@
+extends Node3D
+
+const cube_scene = preload("res://cube.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
